f5d49a98544ca19703c37571393f8a37e623b708
vinaykumargajjela <codewithvinay.dev@gmail.com>
2025-09-13 21:44:17 +0530

    after error with reportweb

---
+++ src/reportWebVitals.js
@@ -0,0 +1,17 @@
+// src/reportWebVitals.js
+// This file is used to measure and report web vitals in a Create React App project.
+// You can customize this file as needed for your analytics or performance monitoring.
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
